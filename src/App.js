96a147561aedfa3ecf5f6cade9215c6ca3f86fc4
Rasool mirbod <rasool_mirbod@yahoo.com>
2021-10-14 19:26:26 +0330

    src/App.js file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product/Product";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: [
+      { title: "React.js", price: "99$",id:1 },
+      { title: "Node.js", price: "89$",id:2 },
+      { title: "Javascript.js", price: "79$",id:3 },
+    ],
+    userDate: "rasool",
+  };
+  clickHandler = () => {};
+  render() {
+    return (
+      <div className="container" id="title">
+        <h1>Shoping App</h1>
+        {this.state.products.map((product) => {
+          return <Product name={product.title} price={product.price} key={product.id} />;
+        })}
+        <button onClick={this.clickHandler}>change price</button>
+      </div>
+    );
+  }
 }
-
 export default App;
