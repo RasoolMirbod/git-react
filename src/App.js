1d878f9b28bce5b6547b3be85fed05671bee598b
Rasool mirbod <rasool_mirbod@yahoo.com>
2021-10-19 08:58:10 +0330

    modoular css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Product from "./components/Product/Product";
 import React, { Component } from "react";
-
+import "./App.css";
 class App extends Component {
   state = {
     products: [
-      { title: "React.js", price: "99$",id:1 },
-      { title: "Node.js", price: "89$",id:2 },
-      { title: "Javascript.js", price: "79$",id:3 },
+      { title: "React.js", price: "99$", id: 1 },
+      { title: "Node.js", price: "89$", id: 2 },
+      { title: "Javascript.js", price: "79$", id: 3 },
     ],
     userDate: "rasool",
   };
@@ -16,7 +16,13 @@ class App extends Component {
       <div className="container" id="title">
         <h1>Shoping App</h1>
         {this.state.products.map((product) => {
-          return <Product name={product.title} price={product.price} key={product.id} />;
+          return (
+            <Product
+              name={product.title}
+              price={product.price}
+              key={product.id}
+            />
+          );
         })}
         <button onClick={this.clickHandler}>change price</button>
       </div>
